6920c374272fd74b78700d0c865be2d608e4df8c
omerfdev <omerfdev@gmail.com>
2023-05-14 00:23:38 +0300

    commit

--- PasswordGeneratorForm/Form1.Designer.cs
+++ PasswordGeneratorForm/Form1.Designer.cs
@@ -34,16 +34,20 @@
             // 
             // txtBoxPassword
             // 
-            txtBoxPassword.Location = new Point(141, 132);
+            txtBoxPassword.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point);
+            txtBoxPassword.Location = new Point(100, 130);
+            txtBoxPassword.MaxLength = 50;
             txtBoxPassword.Name = "txtBoxPassword";
-            txtBoxPassword.Size = new Size(500, 27);
+            txtBoxPassword.PlaceholderText = "Your Password";
+            txtBoxPassword.Size = new Size(550, 27);
             txtBoxPassword.TabIndex = 0;
             txtBoxPassword.TextChanged += textBox1_TextChanged;
             // 
             // lblPasswordShow
             // 
             lblPasswordShow.AutoSize = true;
-            lblPasswordShow.Location = new Point(335, 241);
+            lblPasswordShow.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+            lblPasswordShow.Location = new Point(237, 198);
             lblPasswordShow.Name = "lblPasswordShow";
             lblPasswordShow.Size = new Size(13, 20);
             lblPasswordShow.TabIndex = 1;
@@ -56,9 +60,11 @@
             ClientSize = new Size(782, 353);
             Controls.Add(lblPasswordShow);
             Controls.Add(txtBoxPassword);
+            MaximumSize = new Size(800, 400);
             MinimumSize = new Size(800, 400);
             Name = "Form1";
             Text = "RandomPasswordGenerator";
+            Load += Form1_Load;
             ResumeLayout(false);
             PerformLayout();
         }
--- PasswordGeneratorForm/Form1.cs
+++ PasswordGeneratorForm/Form1.cs
@@ -9,10 +9,15 @@
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            if (txtBoxPassword.Text.Length <= 6) { lblPasswordShow.Text = "Parola Seviyesi Düşük"; lblPasswordShow.ForeColor = Color.Red; }
-            else if (txtBoxPassword.Text.All(char.IsDigit) && txtBoxPassword.Text.All(char.IsLetter)) { lblPasswordShow.Text = "Parola Seviyesi Düşük"; lblPasswordShow.ForeColor = Color.Red; }
-            else if (txtBoxPassword.Text.Length == 7 && txtBoxPassword.Text.Any(char.IsDigit) && txtBoxPassword.Text.Any(char.IsLetter)) { lblPasswordShow.Text = "Parola Seviyesi Orta"; lblPasswordShow.ForeColor = Color.Orange; }
-            else if (txtBoxPassword.Text.Length >= 8 && txtBoxPassword.Text.Any(char.IsLetter) && txtBoxPassword.Text.Any(char.IsDigit)) { lblPasswordShow.Text = "Parola Seviyesi Yüksek"; lblPasswordShow.ForeColor = Color.Green; }
+            if (txtBoxPassword.Text.Length <= 6) { lblPasswordShow.Text = "Password Level is Low"; lblPasswordShow.ForeColor = Color.Red; }
+            else if (txtBoxPassword.Text.All(char.IsDigit) && txtBoxPassword.Text.All(char.IsLetter)) { lblPasswordShow.Text = "Password Level is Low"; lblPasswordShow.ForeColor = Color.Red; }
+            else if (txtBoxPassword.Text.Length == 7 && txtBoxPassword.Text.Any(char.IsDigit) && txtBoxPassword.Text.Any(char.IsLetter)) { lblPasswordShow.Text = "Password Level is Good"; lblPasswordShow.ForeColor = Color.Orange; }
+            else if (txtBoxPassword.Text.Length >= 8 && txtBoxPassword.Text.Any(char.IsLetter) && txtBoxPassword.Text.Any(char.IsDigit)) { lblPasswordShow.Text = "Password Level is Very Good"; lblPasswordShow.ForeColor = Color.Green; }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
