97bed715c77a8d50588dd607a2bb0691122fde0d
omerfdev <omerfdev@gmail.com>
2023-07-08 12:49:59 +0300

    Commit

--- PasswordGeneratorForm/Form1.Designer.cs
+++ PasswordGeneratorForm/Form1.Designer.cs
@@ -1,79 +1,104 @@
 ﻿namespace PasswordGeneratorForm
 {
-    partial class Form1
-    {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class Form1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
-            base.Dispose(disposing);
-        }
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            txtBoxPassword = new TextBox();
-            lblPasswordShow = new Label();
-            SuspendLayout();
-            // 
-            // txtBoxPassword
-            // 
-            txtBoxPassword.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point);
-            txtBoxPassword.Location = new Point(100, 130);
-            txtBoxPassword.MaxLength = 50;
-            txtBoxPassword.Name = "txtBoxPassword";
-            txtBoxPassword.PlaceholderText = "Your Password";
-            txtBoxPassword.Size = new Size(550, 27);
-            txtBoxPassword.TabIndex = 0;
-            txtBoxPassword.TextChanged += textBox1_TextChanged;
-            // 
-            // lblPasswordShow
-            // 
-            lblPasswordShow.AutoSize = true;
-            lblPasswordShow.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
-            lblPasswordShow.Location = new Point(237, 198);
-            lblPasswordShow.Name = "lblPasswordShow";
-            lblPasswordShow.Size = new Size(13, 20);
-            lblPasswordShow.TabIndex = 1;
-            lblPasswordShow.Text = " ";
-            // 
-            // Form1
-            // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(782, 353);
-            Controls.Add(lblPasswordShow);
-            Controls.Add(txtBoxPassword);
-            Icon = (Icon)resources.GetObject("$this.Icon");
-            MaximumSize = new Size(800, 400);
-            MinimumSize = new Size(800, 400);
-            Name = "Form1";
-            Text = "RandomPasswordGenerator";
-            Load += Form1_Load;
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			txtBoxPassword = new TextBox();
+			lblPasswordShow = new Label();
+			txtBoxConfirmPassword = new TextBox();
+			btnLogin = new Button();
+			SuspendLayout();
+			// 
+			// txtBoxPassword
+			// 
+			txtBoxPassword.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point);
+			txtBoxPassword.Location = new Point(142, 94);
+			txtBoxPassword.MaxLength = 50;
+			txtBoxPassword.Name = "txtBoxPassword";
+			txtBoxPassword.PlaceholderText = "Your Password";
+			txtBoxPassword.Size = new Size(478, 27);
+			txtBoxPassword.TabIndex = 0;
+			txtBoxPassword.TextChanged += textBox1_TextChanged;
+			// 
+			// lblPasswordShow
+			// 
+			lblPasswordShow.AutoSize = true;
+			lblPasswordShow.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point);
+			lblPasswordShow.Location = new Point(295, 245);
+			lblPasswordShow.Name = "lblPasswordShow";
+			lblPasswordShow.Size = new Size(13, 20);
+			lblPasswordShow.TabIndex = 1;
+			lblPasswordShow.Text = " ";
+			// 
+			// txtBoxConfirmPassword
+			// 
+			txtBoxConfirmPassword.Font = new Font("Segoe UI", 9F, FontStyle.Italic, GraphicsUnit.Point);
+			txtBoxConfirmPassword.Location = new Point(142, 139);
+			txtBoxConfirmPassword.MaxLength = 50;
+			txtBoxConfirmPassword.Name = "txtBoxConfirmPassword";
+			txtBoxConfirmPassword.PlaceholderText = "Confirm Password";
+			txtBoxConfirmPassword.Size = new Size(478, 27);
+			txtBoxConfirmPassword.TabIndex = 2;
+			// 
+			// btnLogin
+			// 
+			btnLogin.Location = new Point(295, 194);
+			btnLogin.Name = "btnLogin";
+			btnLogin.Size = new Size(99, 34);
+			btnLogin.TabIndex = 3;
+			btnLogin.Text = "Login";
+			btnLogin.UseVisualStyleBackColor = true;
+			btnLogin.Click += btnLogin_Click;
+			// 
+			// Form1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(782, 353);
+			Controls.Add(btnLogin);
+			Controls.Add(txtBoxConfirmPassword);
+			Controls.Add(lblPasswordShow);
+			Controls.Add(txtBoxPassword);
+			Icon = (Icon)resources.GetObject("$this.Icon");
+			MaximumSize = new Size(800, 400);
+			MinimumSize = new Size(800, 400);
+			Name = "Form1";
+			Text = "RandomPasswordGenerator";
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private TextBox txtBoxPassword;
-        private Label lblPasswordShow;
-    }
+		private TextBox txtBoxPassword;
+		private Label lblPasswordShow;
+		private TextBox txtBoxConfirmPassword;
+		private Button btnLogin;
+	}
 }
--- PasswordGeneratorForm/Form1.cs
+++ PasswordGeneratorForm/Form1.cs
@@ -2,31 +2,58 @@
 
 namespace PasswordGeneratorForm
 {
-    public partial class Form1 : Form
-    {
-        public Form1()
-        {
-            InitializeComponent();
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
 
-        }
-        public delegate void PasswordMethodDelegate(TextBox txtBoxPassword, Label lblPasswordShow);
-        public static PasswordMethodDelegate passwordMethodDelegate = PasswordMethod;
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            passwordMethodDelegate(txtBoxPassword, lblPasswordShow);
-        }
+		}
+		public delegate void PasswordMethodDelegate(TextBox txtBoxPassword, TextBox textBoxConfirmPassword, Label lblPasswordShow);
+		public static PasswordMethodDelegate passwordMethodDelegate = PasswordMethod;
+		private void textBox1_TextChanged(object sender, EventArgs e)
+		{
 
-        public static void PasswordMethod(TextBox txtBoxPassword, Label lblPasswordShow)
-        {
-            if (txtBoxPassword.Text.Length <= 6) { lblPasswordShow.Text = "Password Level is Low"; lblPasswordShow.ForeColor = Color.Red; }
-            else if (txtBoxPassword.Text.All(char.IsDigit) && txtBoxPassword.Text.All(char.IsLetter)) { lblPasswordShow.Text = "Password Level is Low"; lblPasswordShow.ForeColor = Color.Red; }
-            else if (txtBoxPassword.Text.Length == 7 && txtBoxPassword.Text.Any(char.IsDigit) && txtBoxPassword.Text.Any(char.IsLetter)) { lblPasswordShow.Text = "Password Level is Good"; lblPasswordShow.ForeColor = Color.Orange; }
-            else if (txtBoxPassword.Text.Length >= 8 && txtBoxPassword.Text.Any(char.IsLetter) && txtBoxPassword.Text.Any(char.IsDigit)) { lblPasswordShow.Text = "Password Level is Very Good"; lblPasswordShow.ForeColor = Color.Green; }
-        }
+		}
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+		public static void PasswordMethod(TextBox txtBoxPassword, TextBox txtBoxConfirmPassword, Label lblPasswordShow)
+		{
+			string password = txtBoxPassword.Text;
+			string confirmPassword = txtBoxConfirmPassword.Text;
 
-        }
-    }
+			if (password.Length > 16)
+			{
+				MessageBox.Show("The password can be up to 16 characters.");
+			}
+			else if (!password.Any(char.IsUpper) && !password.Any(char.IsLower))
+			{
+				MessageBox.Show("Password must contain at least one capital letter.");
+			}
+			else if (!password.Any(char.IsDigit))
+			{
+				MessageBox.Show("The password must contain at least one number.");
+			}
+			else if (!password.Any(c => "_.,!".Contains(c)))
+			{
+				MessageBox.Show("Password is at least one _ . , ! must contain the character.");
+			}
+			else if (password != confirmPassword)
+			{
+				MessageBox.Show("Passwords do not match.");
+			}
+			else
+			{
+				MessageBox.Show($"Your Password {password}");
+				if (txtBoxPassword.Text.Length <= 6) { lblPasswordShow.Text = "Password Level is Low"; lblPasswordShow.ForeColor = Color.Red; }
+				else if (txtBoxPassword.Text.All(char.IsDigit) && txtBoxPassword.Text.All(char.IsLetter)) { lblPasswordShow.Text = "Password Level is Low"; lblPasswordShow.ForeColor = Color.Red; }
+				else if (txtBoxPassword.Text.Length == 7 && txtBoxPassword.Text.Any(char.IsDigit) && txtBoxPassword.Text.Any(char.IsLetter)) { lblPasswordShow.Text = "Password Level is Good"; lblPasswordShow.ForeColor = Color.Orange; }
+				else if (txtBoxPassword.Text.Length >= 8 && txtBoxPassword.Text.Any(char.IsLetter) && txtBoxPassword.Text.Any(char.IsDigit)) { lblPasswordShow.Text = "Password Level is Very Good"; lblPasswordShow.ForeColor = Color.Green; }
+			}
+		}
+
+		private void btnLogin_Click(object sender, EventArgs e)
+		{
+			passwordMethodDelegate(txtBoxPassword, txtBoxConfirmPassword, lblPasswordShow);
+		}
+	}
 }
